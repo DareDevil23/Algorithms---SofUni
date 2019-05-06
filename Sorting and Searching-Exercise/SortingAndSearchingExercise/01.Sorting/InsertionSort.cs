a026c2b30ca2383b7ffefceaba2e2d971f1f3104
Ivaylo Nikolov <ivailonikolov36@gmail.com>
2019-05-07 01:22:50 +0300

    Added Insertion Sort implementation

---
+++ Sorting and Searching-Exercise/SortingAndSearchingExercise/01.Sorting/InsertionSort.cs
@@ -0,0 +1,53 @@
+﻿namespace _01.Sorting
+{
+    using System;
+
+    public class InsertionSort
+    {
+        public static void Sort(int[] array)
+        {           
+            int pivotIndex = 0, index = 1;
+
+            while(index < array.Length)
+            {
+                int currentElement = array[index];
+
+                if (currentElement < array[pivotIndex])
+                {
+                    Insertion(array, pivotIndex, index);
+                }
+
+                pivotIndex++;
+                index++;
+            } 
+        }
+
+        private static void Insertion(int[] array, int pivotIndex, int elementIndex)
+        {
+            int currentElement = array[elementIndex];
+
+            int j = pivotIndex;
+
+            while(j >= 0 && currentElement <= array[j])
+            {
+                j--;       
+            }
+
+            ShiftRight(array, ++j, elementIndex - 1);
+            array[j] = currentElement;
+        }
+
+        private static void ShiftRight(int[] array, int start, int end)
+        {
+            int length = end - start + 1;
+            int[] temp = new int[length];
+            Array.Copy(array, start, temp, 0, length);
+
+            int j = 0;
+            for (int i = start + 1; i <= end + 1; i++)
+            {
+                array[i] = temp[j++];
+            }
+        }
+    }
+}
---
+++ Sorting and Searching-Exercise/SortingAndSearchingExercise/01.Sorting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine()?.Trim();
+
+            if (string.IsNullOrEmpty(inputLine))
+            {
+                return;
+            }
+
+            int[] numbers = inputLine.Split(' ').Select(int.Parse).ToArray();
+
+            InsertionSort.Sort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+    }
+
+}
