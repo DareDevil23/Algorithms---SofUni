d8b234c38a01432b7fea4d3715efb080dba2730c
Ivaylo Nikolov <ivailonikolov36@gmail.com>
2019-05-08 18:05:36 +0300

    Added problems solved with greedy algorithms

---
+++ Greedy Algorithms/Greedy-Algorithms-Lab/SetCover/SetCover.cs
@@ -0,0 +1,57 @@
+﻿namespace SetCover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SetCover
+    {
+        public static void Main(string[] args)
+        {
+            var universe = new[] { 1, 3, 5, 7, 9, 11, 20, 30, 40 };
+            var sets = new[]
+            {
+                new[] { 20 },
+                new[] { 1, 5, 20, 30 },
+                new[] { 3, 7, 20, 30, 40 },
+                new[] { 9, 30 },
+                new[] { 11, 20, 30, 40 },
+                new[] { 3, 7, 40 }
+            };
+
+            var selectedSets = ChooseSets(sets.ToList(), universe.ToList());
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+            foreach (var set in selectedSets)
+            {
+                Console.WriteLine($"{{ {string.Join(", ", set)} }}");
+            }
+        }
+
+        public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+        {
+            HashSet<int[]> setsHash = new HashSet<int[]>(sets);
+            HashSet<int> universeHash = new HashSet<int>(universe);
+
+            List<int[]> resultSets = new List<int[]>();
+
+            while (universeHash.Any())
+            {
+                int[] currentSet = setsHash
+                    .OrderByDescending(s => s.Count(el => universeHash.Contains(el)))
+                    .First();
+
+                resultSets.Add(currentSet);
+
+                foreach (var element in currentSet)
+                {
+                    if (universeHash.Contains(element))
+                    {
+                        universeHash.Remove(element);
+                    }
+                }
+            }
+
+            return resultSets;
+        }
+    }
+}
---
+++ Greedy Algorithms/Greedy-Algorithms-Lab/SumOfCoins/SumOfCoins.cs
@@ -0,0 +1,57 @@
+﻿namespace SumOfCoins
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumOfCoins
+    {
+        public static void Main(string[] args)
+        {
+            var availableCoins = new[] { 1, 2, 5, 10, 20, 50 };
+            var targetSum = 923;
+
+            var selectedCoins = ChooseCoins(availableCoins, targetSum);
+
+            Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+            foreach (var selectedCoin in selectedCoins)
+            {
+                Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
+            }
+        }
+
+        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+        {
+            coins = coins.OrderByDescending(c => c).ToList();
+
+            int currentSum = 0;
+
+            int coinIndex = 0;
+            Dictionary<int, int> coinsCount = new Dictionary<int, int>();
+
+            while (currentSum != targetSum && coinIndex < coins.Count)
+            {
+                int currentCoin = coins[coinIndex];
+                int remainingSum = targetSum - currentSum;
+
+                int numberOfCoinsToTake = remainingSum / currentCoin;
+
+                if (numberOfCoinsToTake != 0 && !coinsCount.ContainsKey(currentCoin))
+                {
+                    coinsCount.Add(currentCoin, numberOfCoinsToTake);
+                }
+
+                currentSum += currentCoin * numberOfCoinsToTake;
+                
+                coinIndex++;
+            }
+
+            if (currentSum < targetSum)
+            {
+                throw new InvalidOperationException("Can't reach target sum.");
+            }
+
+            return coinsCount;
+        }
+    }
+}
