85f1f149f0673f462fd2fe46202e9fe7a916ce24
Ivaylo Nikolov <ivailonikolov36@gmail.com>
2019-05-10 00:24:50 +0300

    Added resolutions for two problems resolvable with greedy algorithm

---
+++ Greedy Algorithms/Greedy-Algorithms-Exercise/GreedyAlgorithmsExercise/1FractionalKnapsack/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _1FractionalKnapsack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = int.Parse(Console.ReadLine().Split(' ')[1]);
+            int itemsCount = int.Parse(Console.ReadLine().Split(' ')[1]);
+
+            IList<Item> items = new List<Item>();
+
+            for (int i = 0; i < itemsCount; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(new []{ "->" }, StringSplitOptions.RemoveEmptyEntries);
+                double price = double.Parse(tokens[0]);
+                int weigth = int.Parse(tokens[1]);
+
+                items.Add(new Item { Price = price, Weigth = weigth });
+            }
+
+            List<Item> sortedItems = items.OrderByDescending(i => i.Price / i.Weigth).ToList();
+
+            int itemIndex = 0;
+
+            double totalPriceTaken = 0f;
+            while(capacity > 0 && itemIndex < sortedItems.Count)
+            {
+                Item currentItem = sortedItems[itemIndex];
+
+                int weightCanTake = Math.Min(capacity, currentItem.Weigth);
+
+                double quantityToTake = (double)weightCanTake / currentItem.Weigth;
+
+                string percentsString = quantityToTake == 1
+                    ? 100.ToString()
+                    : (quantityToTake * 100).ToString("0.00");
+
+                Console.WriteLine(
+                    $"Take {percentsString}% of item with price {currentItem.Price:F2} and weight {currentItem.Weigth:F2}");
+
+                totalPriceTaken += quantityToTake * currentItem.Price;
+                
+                capacity -= weightCanTake;
+
+                if (weightCanTake != currentItem.Weigth)
+                {
+                    currentItem.Weigth -= weightCanTake;
+                    continue;
+                }
+
+                itemIndex++;
+            }
+
+            Console.WriteLine($"Total price: {totalPriceTaken:F2}");
+        }
+    }
+
+    class Item
+    {
+        public double Price { get; set; }
+
+        public int Weigth { get; set; }
+    }
+}
---
+++ Greedy Algorithms/Greedy-Algorithms-Exercise/GreedyAlgorithmsExercise/ProcessorScheduling/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ProcessorScheduling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tasksCount = int.Parse(Console.ReadLine().Split(' ')[1]);
+
+            HashSet<Task> tasks = new HashSet<Task>();
+            for (int i = 0; i < tasksCount; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(new[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+
+                Task task = new Task { Value = int.Parse(tokens[0]), Deadline = int.Parse(tokens[1]) };
+                tasks.Add(task);
+            }
+
+            int maxDeadLine = tasks.Max(t => t.Deadline);
+            List<int> optimalTasks = new List<int>();
+            int totalValue = 0;
+
+            for (int i = 0; i < maxDeadLine; i++)
+            {
+                var mostValuable = tasks
+                    .Where(t => !t.IsFinished && t.Deadline > i)
+                    .OrderByDescending(t => t.Value/(t.Deadline - i))
+                    .First();
+
+                mostValuable.IsFinished = true;
+                totalValue += mostValuable.Value;
+                optimalTasks.Add(mostValuable.InputNumber);
+            }
+
+            Console.WriteLine($"Optimal schedule: {string.Join(" -> ", optimalTasks)}");
+            Console.WriteLine($"Total value: {totalValue}");
+        }
+    }
+
+    class Task
+    {
+        private static int id = 1;
+
+        public int InputNumber { get; set; } = id++;
+
+        public int Value { get; set; }
+
+        public int Deadline { get; set; }
+
+        public bool IsFinished { get; set; }
+    }
+}
