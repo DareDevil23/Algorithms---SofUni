cd29e964c88560bdb5663472a02faceb3cae7449
Ivaylo Nikolov <ivailonikolov36@gmail.com>
2019-05-19 02:15:59 +0300

    Added solutions for Graphs Lab problems

---
+++ Graphs/01. Connected-Components/GraphConnectedComponents.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    static List<int>[] graph;
+    static bool[] visited;
+
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static List<int>[] ReadGraph()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+        }
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        visited = new bool[graph.Length];
+
+        for (int startNode = 0; startNode < graph.Count(); startNode++)
+        {
+            if (!visited[startNode])
+            {
+                Console.Write("Connected component:");
+                Dfs(startNode);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    private static void Dfs(int vertex)
+    {
+        if (!visited[vertex])
+        {
+            visited[vertex] = true;
+            foreach (var child in graph[vertex])
+            {
+                Dfs(child);
+            }
+
+            Console.Write(" " + vertex);
+        }
+    }
+}
---
+++ Graphs/02. Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+    private Dictionary<string, int> predecessorCount;
+
+    private HashSet<string> visited = new HashSet<string>();
+    private HashSet<string> cycleNodes = new HashSet<string>();
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+
+        predecessorCount = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<string, List<string>> node in graph)
+        {
+            if (!predecessorCount.ContainsKey(node.Key))
+            {
+                predecessorCount[node.Key] = 0;
+            }
+
+            foreach (string child in node.Value)
+            {
+                if (!predecessorCount.ContainsKey(child))
+                {
+                    predecessorCount[child] = 0;
+                }
+
+                predecessorCount[child]++;
+            }
+        }
+    }
+
+    //public ICollection<string> TopSort()
+    //{
+    //    List<string> sorted = new List<string>();
+
+    //    while (true)
+    //    {
+    //        string nodeToRemove = predecessorCount.Keys
+    //            .Where(x => predecessorCount[x] == 0)
+    //            .FirstOrDefault();
+
+    //        if (nodeToRemove == null)
+    //        {
+    //            break;
+    //        }
+
+    //        List<string> nodesToDecrementSourceCount = graph[nodeToRemove];
+
+    //        foreach (string item in nodesToDecrementSourceCount)
+    //        {
+    //            predecessorCount[item]--;
+    //        }
+
+    //        graph.Remove(nodeToRemove);
+    //        predecessorCount.Remove(nodeToRemove);
+    //        sorted.Add(nodeToRemove);
+    //    }
+
+    //    if (graph.Count() > 0)
+    //    {
+    //        throw new InvalidOperationException();
+    //    }
+
+    //    return sorted;
+    //}
+
+    public ICollection<string> TopSort()
+    {
+        LinkedList<string> sorted = new LinkedList<string>();
+
+        foreach (var node in graph.Keys)
+        {
+            Dfs(node, sorted);
+        }
+
+        return sorted;
+    }
+
+    private void Dfs(string node, LinkedList<string> sorted)
+    {
+        if (!visited.Contains(node))
+        {
+            visited.Add(node);
+            cycleNodes.Add(node);
+
+            foreach (var child in graph[node])
+            {
+                if (cycleNodes.Contains(child))
+                {
+                    throw new InvalidOperationException("Cycle  detected.");
+
+                }
+                Dfs(child, sorted);
+            }
+
+            cycleNodes.Remove(node);
+            sorted.AddFirst(node);
+        }
+    }
+}
